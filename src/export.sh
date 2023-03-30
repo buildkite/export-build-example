3c0f339aff80cb1faf49035415b6a0d7eef865e6
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-03-30 11:03:13 -0600

    Update export.sh

--- src/export.sh
+++ src/export.sh
@@ -4,12 +4,21 @@ slug_list=()
 
 # Make the curl request to get list of pipelines
 api_url="https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines"
-#option to use "$?" != "200"
-if ! [[$(curl -s -H "Authorization: Bearer $TOKEN" $api_url)>pipelines.json]]; then
-      echo "Error $?: Failed to get pipeline data"
-      exit 1
+response_status=$(curl -s -H "Authorization: Bearer $TOKEN" -w "%{http_code}" -o "pipelines.json" "$api_url")
+
+# Check if the response code is 200 OK
+if [ "$response_status" -ne 200 ]; then
+  echo "Error: API call failed with HTTP status code $response_status"
+  exit 1
 fi
-slugs=jq -r '.[].slug' pipelines.json
+
+# Extract the slug values from the response and add them to the slug list
+if ! slugs=$(jq -r '.[].slug' < "pipelines.json"); then
+  echo "Error: Failed to extract pipeline slug values"
+  exit 1
+fi
+
+# Create a list with all the pipeline slugs
 slug_list+=($slugs)
 
 # Loop through the pipeline slug list and get list of builds for each pipeline slug
@@ -17,13 +26,12 @@ for slug in "${slug_list[@]}"; do
     # Set the list build endpoint URL for the current pipeline slug
     api_url="https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$slug/builds"
 
-    # Make the API request with curl and do something with the response
-    response=$(curl -s -H "Authorization: Bearer $TOKEN" $api_url)
-    # Check if the response is empty or if it contains an error message
-    if [[ -z "$response" || "$response" == *"error"* ]]; then
-       echo "Error: Failed to get build data $response"
+    # Make the API request with curl and generate builds per pipeline in a file
+    response_status=$(curl -s -H "Authorization: Bearer $TOKEN" -w "%{http_code}" -o "pipelines_${slug}.json" "$api_url")
+
+    # Check if the response code is 200 OK
+    if [ "$response_status" -ne 200 ]; then
+       echo "Error: Failed to get build data from API with HTTP status code $response_status"
        exit 1
     fi
-    # Process the response data
-    echo "$response" >> pipelines_$slug.json
 done
