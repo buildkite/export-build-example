f510d8cc379d690e2a9cc9c897ac395e1bdb3f71
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2023-04-03 13:21:42 -0700

    Updated export.sh to support build state filter

--- src/export.sh
+++ src/export.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 slug_list=()
-PAGE_N=1
-PIPELINE_AMOUNT=30
+page=1
+pipeline_length=1
+state="$1"
+valid_states=("running" "scheduled" "passed" "failing" "failed" "blocked" "canceled" "canceling" "skipped" "not_run" "finished")
+
+#Validate input values for state parameter
+if [[ ! " ${valid_states[@]} " =~ " $state " ]]; then
+  echo "Invalid input state: $state. Valid states are: ${valid_states[@]}"
+  exit 1
+fi
 
 # Make the curl request to get list of pipelines
 api_url="https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines"
 
-while [ "${PIPELINE_AMOUNT}" -ne 0 ];do
+while [ "${pipeline_length}" -ne 0 ];do
 
-    response_status=$(curl -s -H "Authorization: Bearer $TOKEN" -w "%{http_code}" -o "pipelines-${PAGE_N}.json" "${api_url}?page=${PAGE_N}")
+  response_status=$(curl -s -H "Authorization: Bearer $TOKEN" -w "%{http_code}" -o "pipelines-${page}.json" "${api_url}?page=${page}")
 
     # Check if the response code is 200 OK
     if [ "$response_status" -ne 200 ]; then
@@ -17,31 +25,42 @@ while [ "${PIPELINE_AMOUNT}" -ne 0 ];do
       exit 1
     fi
 
-    # check how many pipelines finds
-    PIPELINE_AMOUNT=$(jq -r '. | length' "pipelines-${PAGE_N}.json")
+    # Check if more pipelines exist
+    pipeline_length=$(jq -r '. | length' "pipelines-${page}.json")
 
     # Extract the slug values from the response and add them to the slug list
-    if ! slugs=$(jq -r '.[].slug' pipelines-${PAGE_N}.json); then
+    if ! slugs=$(jq -r '.[].slug' pipelines-${page}.json); then
       echo "Error: Failed to extract pipeline slug values"
       exit 1
+    fi
+
+    # Check for empty file and remove it
+    if [ "${pipeline_length}" -eq 0 ]; then
+      rm  pipelines-${page}.json
     fi
 
     # Create a list with all the pipeline slugs
     slug_list+=($slugs)
 
-    PAGE_N=$((PAGE_N + 1))
+    page=$((page + 1))
 done
 
 # Loop through the pipeline slug list and get list of builds for each pipeline slug
 for slug in "${slug_list[@]}"; do
-    # Set the list build endpoint URL for the current pipeline slug
-    api_url="https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$slug/builds"
-    PAGE_N=1
-    BUILD_AMOUNT=30
+    page=1
+    build_length=30
 
-    while [ "${BUILD_AMOUNT}" -ne 0 ];do
+    while [ "${build_length}" -ne 0 ];do
+
+        # Set the list build endpoint URL for the current pipeline slug
+        api_url="https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$slug/builds?page=${page}"
+
+        if [ ! -f "$state" ]; then
+          api_url="https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$slug/builds?state=${state}&page=${page}"
+        fi
+
         # Make the API request with curl and generate builds per pipeline in a file
-        response_status=$(curl -s -H "Authorization: Bearer $TOKEN" -w "%{http_code}" -o "pipelines_${slug}-${PAGE_N}.json" "${api_url}?page=${PAGE_N}")
+        response_status=$(curl -s -H "Authorization: Bearer $TOKEN" -w "%{http_code}" -o "pipelines_${slug}-${page}.json" "${api_url}")
 
         # Check if the response code is 200 OK
         if [ "$response_status" -ne 200 ]; then
@@ -49,10 +68,15 @@ for slug in "${slug_list[@]}"; do
           exit 1
         fi
 
-        BUILD_AMOUNT=$(jq -r '. | length' "pipelines_${slug}-${PAGE_N}.json")
+        build_length=$(jq -r '. | length' "pipelines_${slug}-${page}.json")
 
-        # Copy file to Folder
-        cp pipelines_"${slug}-${PAGE_N}".json pipelines/
-        PAGE_N=$((PAGE_N + 1))
+        # Check for empty file and remove it
+        if [ "${build_length}" -eq 0 ]; then
+          rm  pipelines_${slug}-${page}.json
+        else
+          # Copy file to Folder
+          cp pipelines_"${slug}-${page}".json pipelines/
+        fi
+        page=$((page + 1))
     done
 done
